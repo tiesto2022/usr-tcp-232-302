2c86fc8bb774bd633b8fd70386739fc7cc44848a
tiesto2022 <96581886+tiesto2022@users.noreply.github.com>
2022-03-16 17:31:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 9000
 app.get('/', (req, res) => {
         res.send('OK');
         console.log('Ingreso');
+        
+        /*
         try {
         let net = require('net'),
             host = '170.0.10.203',
@@ -34,6 +36,45 @@ app.get('/', (req, res) => {
 
         });
     }
+    */
+        
+     /* nuevo */
+        // Include Nodejs' net module.
+const Net = require('net');
+// The port number and hostname of the server.
+const port = 20108;
+const host = '170.0.10.203';
+
+// Create a new TCP client.
+const client = new Net.Socket();
+// Send a connection request to the server.
+client.connect({ port: port, host: host }), function() {
+    // If there is no error, the server has accepted the request and created a new
+    // socket dedicated to us.
+    console.log('TCP connection established with the server.');
+
+    // The client can now send data to the server by writing to its socket.
+    client.write('Hello, server.');
+});
+
+// The client can also receive data from the server by reading from its socket.
+client.on('data', function(chunk) {
+    console.log(`Data received from the server: ${chunk.toString()}.`);
+
+    // Request an end to the connection after the data has been received.
+    client.end();
+});
+
+client.on('end', function() {
+    console.log('Requested an end to the TCP connection');
+});
+
+        
+     /* endi nuevo */
+        
+        
+        
+        
 })
 
 app.listen(port,  () => {
