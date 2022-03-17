4937f1640a8a7962b2ff571ba4c15fff4a372abb
tiesto2022 <96581886+tiesto2022@users.noreply.github.com>
2022-03-16 23:00:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,35 +6,40 @@ const port = process.env.PORT || 3000;
 // Modulo incluido en nodejs
 const Net = require('net');
 
-
-app.get('/', (req, res) => {
+pp.get('/', (req, res) => {
     res.send('OK');
-    // Numero de puerto y direccion ip del servidor tcp del convertidor
-    const port = 20108;
-    const host = '170.0.10.203';
+    try {
+        // Numero de puerto y direccion ip del servidor tcp del convertidor
+        const port = 20108;
+        const host = '170.0.10.203';
 
-    // Creo un nuevo cliente TCP
-    const client = new Net.Socket();
+        // Creo un nuevo cliente TCP
+        const client = new Net.Socket();
 
-    // envio una peticion de conexion al servidor
+        // envio una peticion de conexion al servidor
 
-    client.connect({ port: port, host: host }, function() {
-        // Si no hay error el servidor acepta la peticion y
-        // crea un nuevo socket dedicado para nosotros
-        res.json({ msg: 'TCP Conexión establecida con el servidor.' });
-    });
+        client.connect({ port: port, host: host }, function() {
+            // Si no hay error el servidor acepta la peticion y
+            // crea un nuevo socket dedicado para nosotros
+            res.json({ msg: 'TCP Conexión establecida con el servidor.' });
+        });
 
-    // El cliente puede recibir data del servidor y leer desde el socket
-    client.on('data', function(weight) {
-        res.json({ msg: `Data recibida desde el servidor: ${weight.toString()}.` });
+        // El cliente puede recibir data del servidor y leer desde el socket
+        client.on('data', function(weight) {
+            res.json({ msg: `Data recibida desde el servidor: ${weight.toString()}.` });
 
-        // Solicito el final de la petición después de recibir los datos
-        client.end();
-    });
+            // Solicito el final de la petición después de recibir los datos
+            client.end();
+        });
 
-    client.on('end', function() {
-        res.json({ msg: 'Solicito el final de la conexión'});
-    });
+        client.on('end', function() {
+            res.json({ msg: 'Solicito el final de la conexión'});
+        });
+    }
+    catch (e) {
+        res.json({ msg: 'Error '});
+    }
+    
 });
 
 app.listen(port,  () => {
