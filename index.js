7189407acbe3f465e36150e74874e331a11a5769
tiesto2022 <96581886+tiesto2022@users.noreply.github.com>
2022-03-16 23:09:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,40 +8,39 @@ const Net = require('net');
 
 app.get('/', (req, res) => {
     res.json({ message: 'OK' });
-    // try {
-    //     // Numero de puerto y direccion ip del servidor tcp del convertidor
-    //     const port = 20108;
-    //     const host = '170.0.10.203';
-    //
-    //     // Creo un nuevo cliente TCP
-    //     const client = new Net.Socket();
-    //
-    //     // envio una peticion de conexion al servidor
-    //
-    //     client.connect({ port: port, host: host }, function() {
-    //         // Si no hay error el servidor acepta la peticion y
-    //         // crea un nuevo socket dedicado para nosotros
-    //         res.json({ msg: 'TCP Conexión establecida con el servidor.' });
-    //     });
-    //
-    //     // El cliente puede recibir data del servidor y leer desde el socket
-    //     client.on('data', function(weight) {
-    //         res.json({ msg: `Data recibida desde el servidor: ${weight.toString()}.` });
-    //
-    //         // Solicito el final de la petición después de recibir los datos
-    //         client.end();
-    //     });
-    //
-    //     client.on('end', function() {
-    //         res.json({ msg: 'Solicito el final de la conexión'});
-    //     });
-    // }
-    // catch (e) {
-    //     res.json({ msg: 'Error '});
-    // }
+    try {
+        // Numero de puerto y direccion ip del servidor tcp del convertidor
+        const port = 20108;
+        const host = '170.0.10.203';
+
+        // Creo un nuevo cliente TCP
+        const client = new Net.Socket();
+
+        // envio una peticion de conexion al servidor
+
+        client.connect({ port: port, host: host }, function() {
+            // Si no hay error el servidor acepta la peticion y
+            // crea un nuevo socket dedicado para nosotros
+            res.json({ msg: 'TCP Conexión establecida con el servidor.' });
+        });
+
+        // El cliente puede recibir data del servidor y leer desde el socket
+        client.on('data', function(weight) {
+            res.json({ msg: `Data recibida desde el servidor: ${weight.toString()}.` });
+
+            // Solicito el final de la petición después de recibir los datos
+            client.end();
+        });
+
+        client.on('end', function() {
+            res.json({ msg: 'Solicito el final de la conexión'});
+        });
+    }
+    catch (e) {
+        res.json({ msg: 'Error '});
+    }
 
 });
-
 
 app.listen(port,  () => {
     console.log(`Example app listening at http://localhost:${port}`);
