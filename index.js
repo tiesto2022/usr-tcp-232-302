2254ef658b0933a71d5873054a26d6661c7fad3c
tiesto2022 <96581886+tiesto2022@users.noreply.github.com>
2022-03-23 11:40:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get('/', (req, res) => {
 
 app.post('/sendWeight', (req, res) => {
     console.log('sendWeight');
-    console.log({req});    
+    console.log(req.body);    
 });
 
 app.get('/getWeight', (req, res) => {
