1f1ac69e488711ddaea45cf114b2421217a78940
tiesto2022 <96581886+tiesto2022@users.noreply.github.com>
2022-03-24 11:47:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
 // Modulo incluido en nodejs
 const Net = require('net');
 
