891c7347fbb97057bd3baf1c730a10a9c35ea3ec
tiesto2022 <96581886+tiesto2022@users.noreply.github.com>
2022-03-25 09:10:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -75,10 +75,10 @@ app.post('/weight', (req, res) => {
      console.log('Pesando....');
      const values = d.toString().split('=');
      const peso = values[1].trim();
-     console.log({ peso } );      
+     console.log({ peso } );            
+     res.end();
     });   
     
-    res.end();
 });
 
 
