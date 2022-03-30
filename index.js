4c4741c69264cc62cd5ad5f626018acd547126bc
tiesto2022 <96581886+tiesto2022@users.noreply.github.com>
2022-03-30 17:51:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,64 +12,9 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 // Modulo incluido en nodejs
 const Net = require('net');
 
-app.get('/', (req, res) => {
-    console.log('Ingreso a la raiz');
-    try {
-        // Numero de puerto y direccion ip del servidor tcp del convertidor
-        const port = 20108;
-        const host = '170.0.10.200';
-        
-        //const os = require( 'os' );
-        //const networkInterfaces = os.networkInterfaces( );
-        //console.log( networkInterfaces );
-
-        // Creo un nuevo cliente TCP
-        const client = new Net.Socket();
-
-        // envio una peticion de conexion al servidor
-
-        client.connect({ port: port, host: host }, function() {
-            // Si no hay error el servidor acepta la peticion y
-            // crea un nuevo socket dedicado para nosotros
-            console.log('TCP Conexión establecida con el servidor.' );
-        });
-
-        // El cliente puede recibir data del servidor y leer desde el socket
-        client.on('data', function(weight) {
-            const values = weight.toString().split('=');
-            const peso = values[1].trim();
-
-            // Solicito el final de la petición después de recibir los datos
-            client.destroy();
-
-            //res.json({ msg: `Data recibida desde el servidor: ${weight.toString()}.` });
-            console.log({ peso });
-            res.json({ peso });
-        });
-
-        client.on('close', function() {
-            console.log('Solicito el final de la conexión');
-        });
-
-
-        client.on('error', function(err) {
-            console.log(`Error generado: ${err}`);
-        });
-
-
-    }
-    catch (e) {
-        console.log(`Error al ${ e }`)
-    }
-
-});
-
-
 app.post('/weight', async (req, res, next) => {
     console.log("OK");
-    const promesa = await obtenerPesoIndicador(req);
-    console.log(promesa);
-    res.end();
+    res.status(200).end();
     /*   
    
     req.socket.on('data',function(d){
@@ -81,19 +26,6 @@ app.post('/weight', async (req, res, next) => {
     });   */
     
 });
-
-function obtenerPesoIndicador(peticion) {
-    return new Promise((resolve, reject) => {
-        // Apertura del puerto
-        let peso = 0;
-        peticion.socket.on('data',function(d){
-         const values = d.toString().split('=');
-         const peso = values[1].trim();
-         resolve(peso);
-        });         
-    });
- }
-
 
 app.listen(port,  () => {
     console.log(`Example app listening at http://localhost:${port}`);
