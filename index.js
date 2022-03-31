3e62dcc7a3b062034e2b97d4f016eae0584e1fcd
tiesto2022 <96581886+tiesto2022@users.noreply.github.com>
2022-03-31 09:55:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+const capturarPeso = [];
+
 // Modulo incluido en nodejs
 const Net = require('net');
 
 app.post('/weight', async (req, res, next) => {
-    req.on('data', (chunk) => {
-        console.log(`BODY: ${chunk}`);
+    req.on('data', (peso) => {
+        console.log(`BODY: ${peso}`);
+        capturarPeso.push(peso);
     });
     req.on('end', () => {
         console.log('No hay mas data...');
