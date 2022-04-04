3e38586bed31f7610de48ebe3e81468c503e84d6
tiesto2022 <96581886+tiesto2022@users.noreply.github.com>
2022-04-04 10:50:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,6 @@ app.get('/getPeso', async (req, res, next) => {
         error = true;       
         capturarPeso = [];
     }
-    console.log({capturarPeso});
     res.status(200).send({error, peso }); 
         
 });
