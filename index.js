385ed8d489023a56843e7406668b51b4bfd7d071
tiesto2022 <96581886+tiesto2022@users.noreply.github.com>
2022-04-04 12:32:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -44,10 +44,10 @@ app.get('/getPeso', async (req, res, next) => {
         error = true;       
     }
     let data = capturarPeso;
-    let dividir = capturarPeso.join('').replace(" ", "").split("=");
-    let filtrados = dividir.filter(elemento => elemento.length == 6);
+    let unir = capturarPeso.join('');
+    //let filtrados = dividir.filter(elemento => elemento.length == 6);
     
-    res.status(200).send({error, peso, data, dividir, filtrados }); 
+    res.status(200).send({error, peso, data, unir }); 
         
 });
 
